7dada045198e4c926fb2655e04bb55dbe0bfa799
Kaosuchan <2448405043@qq.com>
2023-02-21 19:43:05 +0800

    Update

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,15 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 import tailwind from "@astrojs/tailwind";
+import remarkMath from 'remark-math';
+import rehypeKatex from 'rehype-katex';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://kaosuchan.github.io',
-  integrations: [tailwind()]
+  integrations: [tailwind()],
+  markdown: {
+    remarkPlugins: [remarkMath],
+    rehypePlugins: [rehypeKatex]
+  }
 });
